ed42681a4834ef61b47ea3d0bf5ee704dfd091f6
Khris Punzalan <khrispunzalan@gmail.com>
2019-07-03 12:17:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -24,7 +24,7 @@ class Song
   end
 
   def self.new_by_filename(filename)
-    artist_name, song_name, genre_name = filename.chomp(.mp3).split(" - ")
+    artist_name, song_name, genre_name = filename.chomp(".mp3").split(" - ")
     song = self.new(song_name)
     song.artist = Artist.find_or_create_by_name(artist_name)
     song.genre = genre_name
